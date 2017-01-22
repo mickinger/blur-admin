f140b77b0cff74cbec0cd6f0d21447ae262cb381
michael Wilken <mickinger@web.de>
2017-01-22 12:38:46 +0100

    add config
    mv to tmp

---
+++ config.js
@@ -0,0 +1,14 @@
+
+var config =
+{
+  "name" : "Wessels Amil",
+  "id"   : "456789",
+  "user" : {
+    id : "mark@wasamil",
+    name : "mark",
+    lastname : "Hamil",
+    pass : "openlogin"
+  }
+};
+
+window.myConfig = config;
