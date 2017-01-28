1d87c37ceeab8a128b8d6923ce87539f30d8ae0d
michael Wilken <mickinger@web.de>
2017-01-28 06:15:46 +0100

    chng cnof

---
+++ config0.js
@@ -0,0 +1,15 @@
+
+var config =
+{
+  "name" : "Wessels Amil",
+  "id"   : "456789",
+  "senderType" : 1,
+  "user" : {
+    id : "mark@wasamil",
+    name : "mark",
+    lastname : "Hamil",
+    pass : "openlogin"
+  }
+};
+
+window.myConfig = config;
